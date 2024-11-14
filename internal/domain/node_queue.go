c4c383e3dbe327a60c60292fc70f6deec0f58dda
sojung.kim <thwjd2717@gmail.com>
2024-11-14 21:35:27 +0900

    [#8] feat: 요청을 노드에 전달하는 기능 구현

--- internal/domain/node_queue.go
+++ internal/domain/node_queue.go
@@ -1,10 +1,15 @@
 package domain
 
-import "github.com/zeze1004/LoadBanlence_PoC/config/clouds"
+import (
+	"github.com/zeze1004/LoadBanlence_PoC/config/clouds"
+	"github.com/zeze1004/LoadBanlence_PoC/internal/dto"
+	"sync"
+)
 
 type NodeQueue struct {
 	CloudService *clouds.CloudService
 	Nodes        []*NodeAI
+	Mutex        sync.Mutex // 큐가 노드에게 요청을 전달할 때 동시성 문제를 해결하기 위한 뮤텍스
 	BPM          int
 	RPM          int
 }
@@ -16,4 +21,10 @@ func NewNodeQueue(cloudService *clouds.CloudService, nodes []*NodeAI, BPM, RPM i
 		BPM:          BPM,
 		RPM:          RPM,
 	}
+}
+
+func (nq *NodeQueue) SendRequestToNode(request *dto.Request) {
+	nq.Mutex.Lock()
+	defer nq.Mutex.Unlock()
+	// TODO: 요청을 처리하는 로직 추가
 }
